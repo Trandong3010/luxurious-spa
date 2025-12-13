ff9ff3d343b60a0a6da65b58364f9089fbd4201f
trandong3010 <trandongo1o0@gmail.com>
2025-12-13 23:48:06 +0700

    implement build two-column about section (Introduction text and supporting image/visual)
    feat: Add About section component and display on home page
    
    Implement the new `AboutSection` component and integrate it into the home page.
    This commit introduces the component files and updates `home.html` and `home.ts` to render the About section.
    
    https://github.com/Trandong3010/luxurious-spa/issues/3

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from "../../components/section/hero-banner/hero-banner";
+import { AboutSection } from "../../components/section/about/about";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, AboutSection],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
