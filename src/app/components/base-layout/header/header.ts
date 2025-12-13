9a3f35bcb0aa3ba3739e1432b56e1dc8c8cb77b5
trandong3010 <trandongo1o0@gmail.com>
2025-12-13 21:44:29 +0700

    feat: Implement base layout header with dynamic navigation
    This commit introduces a fully functional and styled header component.
    It includes:
    *   A sticky header with site logo and title.
    *   Dynamic navigation links fetched from updated `route.constants.ts`.
    *   Refactored menu items to include 'Gallery' and 'Pricing', and simplified 'About'.
    *   Added `<base href="/">` to `index.html` for proper Angular routing.

--- src/app/components/base-layout/header/header.ts
+++ src/app/components/base-layout/header/header.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MenuItems } from '../../../constants/route.constants';
+import { Menu } from '../../../models/menu.interface';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-
+  protected menuItems = signal<Menu[]>(MenuItems);
 }
