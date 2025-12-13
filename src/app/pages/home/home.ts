5b9919503a1e3aa1bd613e11b7edb2f05c6f094b
trandong3010 <trandongo1o0@gmail.com>
2025-12-13 23:33:57 +0700

    Implement full-screen Hero Banner (High-quality visual background, compelling headline, and functional CTA button)I
    feat: Integrate HeroBanner component into home page
    
    Update the home page to display the new HeroBanner component.
    This involves importing the Hero component and adding it to the home page's template.
    
    https://github.com/Trandong3010/luxurious-spa/issues/2

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Hero } from "../../components/section/hero-banner/hero-banner";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
