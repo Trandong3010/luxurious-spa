f385dfaabfc0401ae724e8291052297ca495fe3d
trandong3010 <trandongo1o0@gmail.com>
2025-12-14 14:21:55 +0700

    Design and implement team introduction cards (Photo, name, and title for each member)
    feat: Add pricing section to home page
    
    Integrate the new `Pricing` component into the home page. This introduces a dedicated section for displaying pricing plans.
    
    https://github.com/Trandong3010/luxurious-spa/issues/8

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,10 +4,11 @@ import { AboutSection } from '../../components/section/about/about';
 import { ServicesSection } from '../../components/section/services/services';
 import { ImageGallery } from '../../components/section/image-gallery/image-gallery';
 import { Testimonials } from "../../components/section/testimonials/testimonials";
+import { Pricing } from "../../components/section/pricing/pricing";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, AboutSection, ServicesSection, ImageGallery, Testimonials],
+  imports: [Hero, AboutSection, ServicesSection, ImageGallery, Testimonials, Pricing],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
