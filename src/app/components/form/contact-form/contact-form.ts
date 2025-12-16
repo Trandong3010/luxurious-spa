5f732d355b346bc1fe4d85ae7d32d5caf285bd6a
Dong Tran <dong.tran@impartner.com>
2025-12-16 14:25:20 +0700

    Implement contact, inline, and book now forms with reusable input components and error handling.
    https://github.com/Trandong3010/luxurious-spa/issues/23

--- src/app/components/form/contact-form/contact-form.ts
+++ src/app/components/form/contact-form/contact-form.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { InputField } from "../../input-field/input-field";
-import { InputArea } from "../../input-area/input-area";
+import { Component, inject } from '@angular/core';
+import { InputField } from '../../input-field/input-field';
+import { InputArea } from '../../input-area/input-area';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { errorTailorImports } from '@ngneat/error-tailor';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   templateUrl: './contact-form.html',
-  imports: [InputField, InputArea],
+  imports: [InputField, InputArea, ReactiveFormsModule, errorTailorImports],
 })
-export class ContactForm {}
+export class ContactForm {
+  private readonly _fb = inject(FormBuilder);
+
+  form = this._fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', Validators.required],
+    subject: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
--- src/app/components/input-area/input-area.ts
+++ src/app/components/input-area/input-area.ts
@@ -1,24 +1,16 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-area',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './input-area.html',
 })
 export class InputArea {
   label = input<string>('Input Label'); // input for the input label
-  error = input<string>(''); // input for error message
-  value = input<string>(''); // input to store the input value
   placeholder = input<string>('Enter text here...'); // input for placeholder text
   disabled = input<boolean>(false); // input to control input disabled state
   required = input<boolean>(false); // input to control input required state
-  onChange = output<string>();
-
-  // Method to handle input changes
-  protected onInput(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    if (this.onChange) {
-      this.onChange.emit(inputElement.value);
-    }
-  }
+  control = input<FormControl<string | null>>(); // input for form control
 }
